1e53796787a534dcd36f6cf81818a72da9918404
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-27 13:43:20 -0700

    Fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook/config/palette.dart';
-import 'package:flutter_facebook/screens/home_screen.dart';
 import 'package:flutter_facebook/screens/nav_screen.dart';
 
 void main() {
